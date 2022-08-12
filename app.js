61579b18df2ec32c61a76f88a2dc7f3a5c3bf2e9
NicoBolo <nicolascbolo@gmail.com>
2022-08-12 17:33:15 -0300

    Fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const text = "cheating is not allowed"
 const arr = text.replace(/\s/g, '').split("")
 const sqrt = Math.ceil(Math.sqrt(arr.length))
 var newWords = []
-for (i = 0; i <= Math.ceil(arr.length / sqrt); i++) {
+for (i = 0; i < Math.ceil(sqrt); i++) {
     var newArr = []
     for (l = 0; l < arr.length; l = l + sqrt) {
         if (arr[i + l]) {
