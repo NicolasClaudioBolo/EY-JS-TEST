35814335e8ae53bad289c74662d4fe25306dd17d
NicoBolo <nicolascbolo@gmail.com>
2022-08-12 17:30:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const text = "cheating is not allowed"
+const arr = text.replace(/\s/g, '').split("")
+const sqrt = Math.ceil(Math.sqrt(arr.length))
+var newWords = []
+for (i = 0; i <= Math.ceil(arr.length / sqrt); i++) {
+    var newArr = []
+    for (l = 0; l < arr.length; l = l + sqrt) {
+        if (arr[i + l]) {
+            newArr.push(arr[i + l])
+        }
+    }
+    newWords.push(newArr.join(""))
+}
+
+console.log(newWords.join(" "))
